971ab2dfcb513f1405fe71e44ff3023af4412d75
Jean-Michel Fayard <jmfayard@gmail.com>
2021-05-23 08:01:03 +0200

    Switch to ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 import de.fayard.refreshVersions.core.versionFor
+import io.kotless.plugin.gradle.dsl.Webapp.Route53
 import io.kotless.plugin.gradle.dsl.kotless
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
+import io.kotless.resource.Lambda.Config.Runtime
 
 plugins {
     kotlin("jvm")
@@ -12,26 +14,30 @@ group = packageName
 version = "1.0-SNAPSHOT"
 
 repositories {
-    jcenter()
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("io.kotless", "kotless-lang", versionFor("plugin.io.kotless"))
+    //implementation("io.kotless", "ktor-lang-aws", versionFor("plugin.io.kotless"))
+    implementation("io.kotless", "ktor-lang", versionFor("plugin.io.kotless"))
+    implementation(KotlinX.html.jvm)
 }
 
 tasks.withType<KotlinJvmCompile> {
     kotlinOptions {
         jvmTarget = "1.8"
-        languageVersion = "1.3"
-        apiVersion = "1.3"
+        languageVersion = "1.5"
+        apiVersion = "1.5"
     }
 }
 
 kotless {
     config {
         bucket = "jm.kotless.bucket"
+        prefix = "ktor-site"
+
 
         terraform {
             profile = "jm.kotless.user"
@@ -39,10 +45,10 @@ kotless {
         }
     }
     webapp {
+        route53 = Route53("ktor.site", "kotless.io")
+
         lambda {
-            kotless {
-                packages = setOf(packageName)
-            }
+            //runtime = Runtime.GraalVM
         }
     }
 }
